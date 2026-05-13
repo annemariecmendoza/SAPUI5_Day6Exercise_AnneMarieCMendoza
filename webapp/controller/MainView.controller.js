ae0fb6b6751a7f74c056dfa2dfb6df427eee6f1b
Anne Marie Charmaine Mendoza <anne.marie.c.mendoza@accenture.com>
2026-05-13 06:09:00 +0000

    completed activity on fragments and routers

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -52,29 +52,77 @@ sap.ui.define([
                 sap.m.MessageToast.show(sSelectedKeyText);
 
             },
-
+        
+        /*
         onPressCheckout: function (){ 
 
             var oInputFNameValue = this.getView().byId("idInptFName").getValue(); 
             var oInputLNameValue = this.getView().byId("idInptLName").getValue(); 
 
             // Check if first name is blank 
-            /*if (oInputFNameValue === ""){ 
-                sap.m.MessageToast.show("Required Field is blank");  
-            }*/ 
+            //if (oInputFNameValue === ""){ 
+            //    sap.m.MessageToast.show("Required Field is blank");  
+            //}
 
             //Check if first and last names are blank
             if (oInputFNameValue === "" && oInputLNameValue === "" ){ 
                 sap.m.MessageToast.show("First and Last Names are blank");  
             }
+        
+        },*/
 
-        },
+        onPressCheckout: function (){ 
+
+            var oInputFName = this.getView().byId("idInptFName"); 
+            var oInputLName = this.getView().byId("idInptLName"); 
+
+            var oInputFNameValue = oInputFName.getValue(); 
+            var oInputLNameValue = oInputLName.getValue(); 
+
+            var oRouter = this.getOwnerComponent().getRouter(); 
+
+            // Check if first name and last name is blank 
+            if (oInputFNameValue === "" || oInputLNameValue === ""){ 
+                // set value state to Error 
+                oInputFName.setValueState("Error"); 
+                oInputLName.setValueState("Error"); 
+            } else { 
+                oInputFName.setValueState("None"); 
+                oInputLName.setValueState("None"); 
+
+                //Navigate to review page passing first 
+                oRouter.navTo("RouteReviewPage", { firstName: oInputFNameValue }); 
+            } 
+
+        }, 
 
         onAddItem: function (){ 
-                var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle(); 
-                var sMsg = oTextBundle.getText("addButtonMsg"); 
-                this.fnDisplayMsg(sMsg); 
-            } 
+                // Comment this code for now 
+                // var oTextBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle(); 
+                // var sMsg = oTextBundle.getText("addButtonMsg"); 
+                // this.fnDisplayMsg(sMsg);     
+
+                /*---------------------------Fragments and Routers part--------------------------*/   
+                // Instantiate the fragment
+                // create dialog lazily 
+
+                if (!this.oDialog) { 
+
+                    // By using loadFragment, we are adding the fragment as a dependent to the View 
+                    // By doing so, we can use the functions inside the view's controller 
+                    this.oDialog = this.loadFragment({ 
+                        name: "com.training.exer5mendoza.fragment.ProductDialog" 
+                    }); 
+                }  
+                    this.oDialog.then(function(oDialog) { 
+                    oDialog.open(); 
+                }); 
+
+        },
+        
+        onCloseDialog: function (){ 
+            this.getView().byId("idProductDialog").close(); 
+        }, 
 
     });
 });
